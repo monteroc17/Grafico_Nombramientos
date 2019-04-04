f88b34c2277a5ffd8610a85e458c260d6e28a685
Daniel Montero <d.montero95@gmail.com>
2019-04-04 10:00:38 -0600

    Scroll listo

--- javascript/main.js
+++ javascript/main.js
@@ -244,6 +244,36 @@ function obtenerMayor(lista) {
     return elementoMax;
 }
 
+function scroll()
+{
+    const slider = document.getElementById('chartDiv');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', (e) => {
+      isDown = true;
+      slider.classList.add('active');
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener('mouseleave', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mouseup', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mousemove', (e) => {
+      if(!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX) * 2;
+      slider.scrollLeft = scrollLeft - walk;
+    });
+}
+
 
 let buttonDia = document.getElementById('btnDia');
 let buttonMes = document.getElementById('btnMes');
@@ -254,6 +284,7 @@ let buttonAno = document.getElementById('btnAno');
 // buttonAno.onclick = cargarGrafico(filtrarPorAño());
 
 cargarGraficoPorDias(datos);
+scroll();
 
 
 
